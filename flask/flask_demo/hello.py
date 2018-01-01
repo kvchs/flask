1b28116573967d5fb97d4363270217437412d55d
kvchs <15680488752@163.com>
2018-01-01 14:13:47 +0800

    add requirement

--- flask/flask_demo/hello.py
+++ flask/flask_demo/hello.py
@@ -4,29 +4,44 @@ from flask import make_response
 from flask import redirect
 from flask import abort
 from flask_script import Manager
+from flask_moment import Moment
+from flask import Flask,render_template
+from flask_bootstrap import Bootstrap
+from datetime import datetime
 
 app = Flask(__name__)
 #manager = Manager(apppp)
 #app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.route('/')
 def index():
-    return redirect("http://www.baidu.com")
-    #abort(404)
+    return render_template('index.html', current_time=datetime.utcnow())
 
 
-@app.route('/user/<id>')
-def get_user(id):
-    user = load_user(id)
-    if not user:
-        abort(404)
-    return '<h2>Hello ,%s<h2>' % user.name
+# @app.route('/user/<id>')
+# def get_user(id):
+#     user = load_user(id)
+#     if not user:
+#         abort(404)
+#     return '<h2>Hello ,%s<h2>' % user.name
 
 
 @app.route('/user/<name>')
 def user(name):
-    return "<h2>Hello,%s!</h2>" % name
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_find(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 if __name__ == "__main__":
